90e86a503406d63aac8fde9dbfcf11aee5e95075
steima <matthias@matthias.mbp.local>
2016-12-15 13:06:40 +0100

    Removes unneeded files

--- key-injection.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /root/.ssh
-echo >> /root/.ssh/authorized_keys
-curl -m 10 -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key | grep 'ssh-rsa' >> /root/.ssh/authorized_keys
-echo "AUTHORIZED_KEYS:"
-echo "************************"
-cat /root/.ssh/authorized_keys
-echo "************************"
--- show-db-ip.sh
+++
@@ -1,5 +0,0 @@
-SERVERIP="`/usr/bin/curl -m 60 -s http://169.254.169.254/openstack/latest/meta_data.json | /usr/bin/jq -r '.meta.dbserverip'`"
-
-/usr/bin/curl -m 60 -s http://169.254.169.254/openstack/latest/meta_data.json >> /tmp/meta_data.json
-
-echo "<html><head><title>DB Server Private IP</title></head><body>${SERVERIP}</body></html>" > /var/www/html/ip.html
